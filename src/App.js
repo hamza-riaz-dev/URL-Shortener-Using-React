15c9b93ddfcd6e19626d8c6babb6c76e321def46
ShahMir Ali <shahmirali683@gmail.com>
2023-05-08 22:33:14 +0500

    Added Componenets to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import BackgroundAnimate from './BackgroundAnimate';
+import HistoryButton from './HistoryButton';
+import Home from './Home';
 
 function App() {
   return (
     <div className="container">
-
+    <Home/>
+    <BackgroundAnimate></BackgroundAnimate>
+    <HistoryButton/>
     </div>
   );
 }
